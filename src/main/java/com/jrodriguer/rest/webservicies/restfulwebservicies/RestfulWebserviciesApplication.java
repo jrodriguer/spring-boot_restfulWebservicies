9cc037ba0df710df5f2413bb5725cc5807be59db
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-04-21 21:11:44 +0200

    initial commit

---
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/RestfulWebserviciesApplication.java
@@ -0,0 +1,13 @@
+package com.jrodriguer.rest.webservicies.restfulwebservicies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebserviciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebserviciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrodriguer/rest/webservicies/restfulwebservicies/RestfulWebserviciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrodriguer.rest.webservicies.restfulwebservicies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebserviciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
