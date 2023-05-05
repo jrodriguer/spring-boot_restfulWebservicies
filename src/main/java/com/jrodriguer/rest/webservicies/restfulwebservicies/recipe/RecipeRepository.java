ed2d2ba8e39ac376bf38325373baf8f4f6b24ac9
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-05-05 21:59:03 +0200

    create interface that extends Jpa

---
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipe/RecipeRepository.java
@@ -0,0 +1,8 @@
+package com.jrodriguer.rest.webservicies.restfulwebservicies.recipe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+}
