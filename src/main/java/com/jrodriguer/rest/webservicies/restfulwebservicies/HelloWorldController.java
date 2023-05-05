48db732ed7e10000fe50fc873dc587e381f14ec1
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-05-05 18:54:39 +0200

    testing with path variable method

--- src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/HelloWorldController.java
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/HelloWorldController.java
@@ -1,11 +1,12 @@
 package com.jrodriguer.rest.webservicies.restfulwebservicies;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	
+
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
@@ -14,6 +15,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 
 }
