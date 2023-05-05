80aceae8c3e383e1b9490b9c11650ffb53ebf14d
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-05-05 22:02:26 +0200

    create Repository property and add to constructor

--- src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipe/RecipeService.java
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipe/RecipeService.java
@@ -2,8 +2,17 @@ package com.jrodriguer.rest.webservicies.restfulwebservicies.recipe;
 
 import java.util.List;
 
-public class RecipeService {
+import org.springframework.stereotype.Service;
 
+@Service
+public class RecipeService {
+	
+	private final RecipeRepository recipeRepository;
+	
+	public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+	
 	public List<Recipe> getAllRecipes() {
 		// TODO Auto-generated method stub
 		return null;
