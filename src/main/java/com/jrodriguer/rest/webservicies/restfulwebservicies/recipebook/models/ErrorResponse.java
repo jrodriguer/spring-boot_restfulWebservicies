c07dc145b4ea05ab0df38a99dc272dcfa4bb0593
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-05-31 19:59:29 +0200

    add constructor

--- src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/models/ErrorResponse.java
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/models/ErrorResponse.java
@@ -4,4 +4,10 @@ public class ErrorResponse {
   private int status;
   private String error;
   private String message;
+
+  public ErrorResponse(int status, String error, String message) {
+    this.status = status;
+    this.error = error;
+    this.message = message;
+  }
 }
