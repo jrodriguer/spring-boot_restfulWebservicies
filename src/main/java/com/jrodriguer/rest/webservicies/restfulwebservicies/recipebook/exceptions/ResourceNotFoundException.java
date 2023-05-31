7f6011526eb442faa737bf10982c1f0a8a2ddcf0
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-05-31 20:02:08 +0200

    using the ErrorResponse

--- src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,18 @@
 package com.jrodriguer.rest.webservicies.restfulwebservicies.recipebook.exceptions;
+import com.jrodriguer.rest.webservicies.restfulwebservicies.recipebook.models.ErrorResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     
   public ResourceNotFoundException(String message) {
     super(message);
+  }
+
+  public ErrorResponse getErrorResponse() {
+    return new ErrorResponse(HttpStatus.NOT_FOUND.value(), "Resource not found", getMessage());
   }
 
 }
