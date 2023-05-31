cdd538842fff0833952b1456479b69c592b704be
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-05-31 19:40:43 +0200

    class to represent the structure of the error response

---
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/models/ErrorResponse.java
@@ -0,0 +1,7 @@
+package com.jrodriguer.rest.webservicies.restfulwebservicies.recipebook.models;
+
+public class ErrorResponse {
+  private int status;
+  private String error;
+  private String message;
+}
