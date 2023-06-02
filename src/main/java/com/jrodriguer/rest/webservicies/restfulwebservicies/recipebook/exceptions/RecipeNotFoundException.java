cd79d2b1e00b8e72c59ef4890afb2ef025ac699e
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-06-02 21:52:27 +0200

    rename file, focus on recipe

---
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/exceptions/RecipeNotFoundException.java
@@ -0,0 +1,13 @@
+package com.jrodriguer.rest.webservicies.restfulwebservicies.recipebook.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RecipeNotFoundException extends RuntimeException {
+    
+  public RecipeNotFoundException(String message) {
+    super(message);
+  }
+
+}
