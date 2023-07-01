cee35f1de66622bfb6e9ad15b449a7f2cb2a266c
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-07-01 17:06:24 +0200

    remove unnecessary code

--- src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/RecipeSpringDataJpaRepository.java
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/RecipeSpringDataJpaRepository.java
@@ -1,7 +1,5 @@
 package com.jrodriguer.rest.webservicies.restfulwebservicies.recipebook;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +7,4 @@ import com.jrodriguer.rest.webservicies.restfulwebservicies.recipebook.models.Re
 
 @Repository
 public interface RecipeSpringDataJpaRepository extends JpaRepository<Recipe, Long> {
-
-  List<Recipe> findByName(String name);
-
 }
