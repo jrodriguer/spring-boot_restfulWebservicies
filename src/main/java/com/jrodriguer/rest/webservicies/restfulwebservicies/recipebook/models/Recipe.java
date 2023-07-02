a015444efac082357509dccc6523170a0a8ba174
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-07-02 21:14:31 +0200

    refactor for spring propertie

--- src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/models/Recipe.java
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/models/Recipe.java
@@ -2,6 +2,8 @@ package com.jrodriguer.rest.webservicies.restfulwebservicies.recipebook.models;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
@@ -13,28 +15,32 @@ import jakarta.persistence.Id;
 @Entity(name = "RECIPE")
 public class Recipe {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    protected Recipe() {
+    }
 
-    @Column(name = "RECIPE_NAME", length = 50, nullable = false, unique = false)
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "RECIPE_NAME", nullable = false, unique = false)
+    @JsonProperty("recipe_name")
     private String name;
 
     @Column(name = "RECIPE_DESCRIPTION", nullable = false, unique = false)
+    @JsonProperty("recipe_description")
     private String description;
 
     @ElementCollection
     @CollectionTable(name = "RECIPE_INGREDIENTS")
+    @JsonProperty("recipe_ingredients")
     private List<String> ingredients;
 
-    public Recipe() {
-    }
-
-    public Recipe(Long id, String name, String description) {
+    public Recipe(Integer id, String name, String description, List<String> ingredients) {
         super();
         this.id = id;
         this.name = name;
         this.description = description;
+        this.ingredients = ingredients;
     }
 
 }
