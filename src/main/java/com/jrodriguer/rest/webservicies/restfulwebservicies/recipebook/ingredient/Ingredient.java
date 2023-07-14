ffeedb69f502aa4c0b7b915b7df3ab0eb7a67905
jrodriguer <juliorodriguezramirez@gmail.com>
2023-07-14 09:47:41 +0200

    import Recipe for Ingredient

--- src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/ingredient/Ingredient.java
+++ src/main/java/com/jrodriguer/rest/webservicies/restfulwebservicies/recipebook/ingredient/Ingredient.java
@@ -2,6 +2,7 @@ package com.jrodriguer.rest.webservicies.restfulwebservicies.recipebook.ingredie
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.jrodriguer.rest.webservicies.restfulwebservicies.recipebook.recipe.Recipe;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
